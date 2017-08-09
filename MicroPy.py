2197c4ff05d73e21ed58ccb934716c10cca44558
Andrew Spittlemeister <andyspitts@gmail.com>
2017-08-09 11:09:53 -0500

    Added MicroPyPID and reworked ready status checks

--- MicroPy.py
+++ MicroPy.py
@@ -71,7 +71,6 @@ class MicroPyInterrupt:
         self.run = True
         self.timer.start()
 
-
 class MicroPyStateMachine:
     """
     Class to aid in using state machines with defined functions for each state.
@@ -133,7 +132,7 @@ class MicroPyStateMachine:
                 self.internalLooping = False
                 return
             sleep(self.loopDelay)
-           
+
 class MicroPyBuffer:
     """
     Class wrapper for a simple circular buffer.
@@ -152,21 +151,24 @@ class MicroPyBuffer:
 
     def typeCheck(self):
         """
-        CHecks if necessary fields for the buffer are of the correct type. Will set ready status to true if all fields
+        Checks if necessary fields for the buffer are of the correct type. Will set ready status to true if all fields
             meet the requirements.
         :return: Nothing.
         """
+        self.ready = True
+
         if not type(self.length) is int:
             print("Error: invalid length argument type.")
             self.ready = False
-        elif not type(self.overwrite) is type(False):
+        if not type(self.overwrite) is type(False):
             print("Error: invalid overwrite argument type.")
             self.ready = False
-        elif not type(self.readChronological) is type(False):
+        if not type(self.readChronological) is type(False):
             print("Error: invalid readChronological argument type.")
             self.ready = False
-        else:
-            self.ready = True
+
+        print("Ready Status: ", self.ready)
+
 
     def initBuffer(self):
         """
@@ -175,7 +177,7 @@ class MicroPyBuffer:
         """
         for i in range(self.length):
             self.buffer[i] = None
-    
+
     def reset(self):
         """
         Resets buffer to initial conditions and resets cursors to 0.
@@ -197,8 +199,10 @@ class MicroPyBuffer:
         Returns True if there is any data in the buffer, otherwise False.
         :return: Boolean.
         """
-        if not self.ready:
+        if not self.ready:  # check ready status
+            print("Error: not ready to execute.")
             return False
+
         if self.buffer[self.reader] is None:
             return False
         else:
@@ -209,33 +213,39 @@ class MicroPyBuffer:
         Returns True if there is an open spot to write data in the buffer or if overwrite is allowed.
         :return: Boolean.
         """
-        if not self.ready:
+        if not self.ready:  # check ready status
+            print("Error: not ready to execute.")
             return False
+
         if self.overwrite:
             return True
         elif self.buffer[self.writer] is None:
             return True
         else:
             return False
-        
+
     def toggleOverwrite(self):
         """
         Toggles overwrite status.
         :return: Nothing.
         """
-        if not self.ready:
-            return
+        if not self.ready:  # check ready status
+            print("Error: not ready to execute.")
+            return False
+
         self.overwrite = not self.overwrite
-        
+
     def toggleReadChronological(self):
         """
         Toggles readChronological status.
         :return: Nothing.
         """
-        if not self.ready:
-            return
+        if not self.ready:  # check ready status
+            print("Error: not ready to execute.")
+            return False
+
         self.readChronological = not self.readChronological
-        
+
     def add(self, data):
         """
         Adds piece of data to the next writeable spot in the buffer, then increments writer cursor. If failed, cursor
@@ -243,8 +253,10 @@ class MicroPyBuffer:
         :param data: Data to be added.
         :return: True for success, False for failure.
         """
-        if not self.ready:
+        if not self.ready:  # check ready status
+            print("Error: not ready to execute.")
             return False
+
         if self.buffer[self.writer] is None:
             self.buffer[self.writer] = data
             self.writer = (self.writer + 1) % self.length
@@ -263,8 +275,10 @@ class MicroPyBuffer:
         Gets next available piece of data in the buffer.
         :return: Data found or None. Returns False if ready status is not met.
         """
-        if not self.ready:
+        if not self.ready:  # check ready status
+            print("Error: not ready to execute.")
             return False
+
         if self.buffer[self.reader] is None:
             return None
         else:
@@ -278,8 +292,10 @@ class MicroPyBuffer:
         Jumps reader to most recently written data in buffer and sets all other values to None.
         :return: Nothing.
         """
-        if not self.ready:
-            return
+        if not self.ready:  # check ready status
+            print("Error: not ready to execute.")
+            return False
+
         newIndex = self.reader - 1
         if newIndex < 0:
             newIndex = self.length - 1
@@ -287,3 +303,124 @@ class MicroPyBuffer:
             if not i == newIndex:
                 self.buffer[i] = None
         self.reader = newIndex
+
+
+class MicroPyPID:
+    """
+    Class wrapper for defining and running a PID control loop.
+    """
+    def __init__(self, setpoint, CObias, timeDelta = None, upperSumBound = None, lowerSumBound = None, Kc = None, Ki = None, Kd = None):
+        self.setpoint = setpoint
+
+        self.CObias = CObias
+        self.Kc = Kc
+        self.Ki = Ki
+        self.Kd = Kd
+
+        self.timeDelta = timeDelta
+        self.prevError = 0
+        self.errorSum = 0
+        self.upperSumBound = upperSumBound
+        self.lowerSumBound = lowerSumBound
+
+        self.loopType = self.getLoopType()
+
+        self.ready = None
+        self.typeCheck()
+
+    def getLoopType(self):
+        """
+        Returns the control loop type (P, PI, or PID) based on what parameters were given.
+        :return: String containing control loop type.
+        """
+        loopType = ""
+        if self.Kc is not None:
+            loopType += 'P'
+            if self.Ki is not None:
+                loopType += 'I'
+                if self.kd is not None:
+                    loopType += 'D'
+        return loopType
+
+    def typeCheck(self):
+        """
+        Verifies that required parameters for the specified control loop type are of the correct type.
+        :return: Nothing.
+        """
+        self.ready = True
+
+        if not (self.loopType == "P" or self.loopType == "PI" or self.loopType == "PID"):
+            print("Error: invalid control loop type.")
+            self.ready = False
+        if not (type(self.setpoint) is float or type(self.setpoint) is int):
+            print("Error: invalid setpoint type.")
+            self.ready = False
+        if not (type(self.CObias) is float or type(self.CObias) is int):
+            print("Error: invalid CObias type.")
+            self.ready = False
+
+        # check PID constants according to control loop type:
+        if 'P' in self.loopType:  # check Kc parameter
+            if not (type(self.Kc) is float or type(self.Kc) is int):
+                print("Error: invalid Kc type.")
+                self.ready = False
+
+            if 'I' in self.loopType:  # check Ki parameter + time delta + sum bound
+                if not (type(self.Ki) is float or type(self.Ki) is int):
+                    print("Error: invalid Ki type.")
+                    self.ready = False
+                if not (type(self.timeDelta) is float or type(self.timeDelta) is int):
+                    print("Error: invalid timeDelta type.")
+                    self.ready = False
+                if not (type(self.upperSumBound) is float or type(self.upperSumBound) is int):
+                    print("Error: invalid upperSumBound type.")
+                    self.ready = False
+                if not (type(self.lowerSumBound) is float or type(self.lowerSumBound) is int):
+                    print("Error: invalid lowerSumBound type.")
+                    self.ready = False
+
+                if 'D' in self.loopType:  # check Kd parameter
+                    if not (type(self.Kd) is float or type(self.Kd) is int):
+                        print("Error: invalid Kd type.")
+                        self.ready = False
+
+        print("Ready Status: ", self.ready)
+
+    def getOutput(self, reading):
+        """
+        Calculates new controller output based on control loop type.
+        :return: Controller output (float) or False (bool) when not ready or invalid input.
+        """
+        if not self.ready:  # check ready status
+            print("Error: not ready to execute.")
+            return False
+
+        if not (type(reading) is float or type(reading) is int):  # check if reading is of valid type
+            print("Error: invalid reading type.")
+            return False
+
+        output = self.CObias
+
+        if 'P' in self.loopType:  # contribute proportional term
+            current_error = self.setpoint - reading
+            output += self.Kc * current_error
+
+            if 'I' in self.loopType:  # contribute integral term
+                self.errorSum += current_error
+
+                # format sum to be within defined range:
+                if self.errorSum < self.lowerSumBound:
+                    self.errorSum = self.lowerSumBound
+                elif self.errorSum > self.upperSumBound:
+                    self.errorSum = self.upperSumBound
+
+                output += self.Ki * self.errorSum * self.timeDelta
+
+                if 'D' in self.loopType:  # contribute derivative term
+                    errorDelta = current_error - self.prevError
+                    output += errorDelta / self.timeDelta
+
+            self.prevError = current_error
+
+        return output
+
